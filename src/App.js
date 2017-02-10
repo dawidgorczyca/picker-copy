e96ad3d4275be2cefb806167d202dc74a4e240ae
Dawid Gorczyca <Dawid Gorczyca>
2017-02-10 13:24:36 +0100

    added basic picker

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { heroesList } from './TypesAndDefaults';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      playerName: '',
+      hero: '',
+    }
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.changeHero = this.changeHero.bind(this)
+  }
+
+  handleInputChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  changeHero(hero) {
+    this.setState({ hero: hero });
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="App picker">
+        <div className="picker--portrait">
+          <img src={logo} className="picker--portrait-dummy" alt="portrait" />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="picker--hero-data">
+          <ul className="picker--portrait--heroes-list">
+            {heroesList.map((hero, index) =>
+              <li key={index} className="heroes-list--item" onClick={() => {this.changeHero(hero)}}>
+                <span className="heroes-list--item--age">{hero.age}</span>
+                <span className="heroes-list--item--name">{hero.name}</span>
+              </li>
+            )}
+          </ul>
+        </div>
+        <div className="picker--player-data">
+          <input type="text" name="playerName" value={this.state.playerName} onChange={this.handleInputChange}/>
+        </div>
       </div>
     );
   }
---
+++ src/TypesAndDefaults.js
@@ -0,0 +1,14 @@
+export const heroesList = [
+    {
+        name: 'Hero 1',
+        age: '24',
+    },
+    {
+        name: 'Hero 2',
+        age: '36',
+    },
+    {
+        name: 'Hero 3',
+        age: '48',
+    },
+]
